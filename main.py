f7f7eaff6085b2a3b6ff89924d7bc33f51a989e5
Isaiah <ifdsisd@gmail.com>
2022-03-26 15:36:24 -0500

    added to sys

---
+++ main.py
@@ -0,0 +1,45 @@
+from time import process_time_ns
+from tkinter import *
+from tkinter import ttk
+import tkinter
+from turtle import bgcolor
+import modules
+
+from click import command
+
+def retrieve_input():
+    print(hash_var.get())
+    print(path_var.get())
+    ttk.Label(frm,font=("Comic Sans", 20),text=hash_var.get(),foreground=('RED')).grid(column=1,row=2) 
+
+
+
+
+
+tk = tkinter
+root = Tk()
+root.title("Hash comparator")
+
+
+frm = ttk.Frame(root,padding=10)
+
+frm.grid()
+
+hash_var = tk.StringVar()
+path_var = tk.StringVar()
+
+
+ttk.Label(frm, text="Enter Hash:\t").grid(column=0, row=0) 
+hash_text = Entry(frm,textvariable=hash_var)
+hash_text.grid(column=1,row=0)
+ttk.Label(frm, text="Enter Path:\t").grid(column=0, row=1) 
+path_text  = Entry(frm,textvariable=path_var)
+path_text.grid(column=1,row=1)
+
+ttk.Label(frm,text="Executable Status:\t").grid(column=0,row=2)
+
+
+
+ttk.Button(frm, text="Check", command=retrieve_input).grid(column=2, row=2)
+
+root.mainloop()
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,6 @@
 # import modules
+<<<<<<< Updated upstream
 from modules import debian_hash
+=======
+from modules import debian_hash
+>>>>>>> Stashed changes
