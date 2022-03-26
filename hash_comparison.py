441d709814338f76239106575bf3da185d43e733
Jose Soraiz <38092188+Soraiz@users.noreply.github.com>
2022-03-26 14:58:24 -0500

    Update hash_comparison.py

---
+++ hash_comparison.py
@@ -0,0 +1,11 @@
+#python project to store the checksums for comparison
+import os 
+from os.path import isfile, join
+
+file_path = input("Enter file path: ")
+
+#base_path = r"C:\users\josea\Documents\miscellanus"
+base_path = file_path
+file_ls = [f for f in os.listdir(base_path) if isfile(join(base_path, f))]
+print(file_ls)
+
