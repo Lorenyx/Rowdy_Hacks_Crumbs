29639adb87ad51a3d1571a7b865f2dc20b0739a1
Isaiah <ifdsisd@gmail.com>
2022-03-26 16:08:55 -0500

    added recently edited warning

---
+++ modules/__pycache__/recently_edited.py
@@ -0,0 +1,8 @@
+import os.path
+
+def recently_edited(str: bin):
+    "warns users if the bin has recently been updated"
+    if (os.path.isfile(bin)):
+        print(placeholder)
+    else:
+        return -1
