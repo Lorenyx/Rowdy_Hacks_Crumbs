6742565dd4a70aa8b91d7685c9af5971882b8ba1
Isaiah <ifdsisd@gmail.com>
2022-03-26 20:44:24 -0500

    completed recently_edited

--- modules/__pycache__/recently_edited.py
+++ modules/__pycache__/recently_edited.py
@@ -1,12 +1,19 @@
 import os.path
 import time
+from datetime import date, datetime
 
 def recently_edited(bin:str):
     "warns users if the bin has recently been updated"
     path="/usr/bin/{}".format(bin)
     if (os.path.isfile("/usr/bin/"+bin)):
-        modified = time.ctime(os.path.getmtime(path))
+        modified = date.fromtimestamp(os.path.getmtime(path))
+        current = date.fromtimestamp(time.time())
+        elapsed = current-modified
+        if (elapsed.days < 14):
+            print("Friendly reminder {} has been modified within 14 days of this program running")
+    
+        
     else:
-        return -1
+        return 1
 if __name__ == "__main__":
     recently_edited("neofetch")
