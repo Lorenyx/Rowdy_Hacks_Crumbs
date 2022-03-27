11169782003b43503d89ecf32d1220ef7f7f3252
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-03-26 19:25:10 -0500

    Refactor hash_comparison

--- hash_comparison.py
+++ hash_comparison.py
@@ -3,7 +3,6 @@ import os
 from os.path import isfile, join
 import hashlib
 
-
 BLOCK_SIZE = 65536
 
 # file_path = input("Enter file path: ")
@@ -13,9 +12,33 @@ BLOCK_SIZE = 65536
 
 # for f in file_ls:
 #     print(f)
+def get_hash(path, hash_algo):
+    # provided Path is a file
+    if os.path.isfile(path):
+        # Comparse Hash to provided one
+        print(f'*** Decoding file {path} into {hash_algo}')
+        get_hash_from_file(path, hash_algo)
+
+    if os.path.isdir(path):
+        # call get_all_file_Hash
+        print(f'*** Decoding dir {path} into {hash_algo}')
+        get_hash_from_dir(path, hash_algo)
 
 
-def get_file_hash(file_path, hash_algo):
+
+def get_hash_from_dir(dir_path, hash_algo):
+    """Returns list of hashes for all files in directory"""
+    if not os.path.isdir(dir_path):
+        print(f"[-] Cannot find directory at {dir_path}")
+        return
+
+    hashes = []
+    for file in os.listdir(dir_path):
+        hashes.append(get_hash_from_file(os.path.join(dir_path, file), hash_algo))
+    return hashes
+
+
+def get_hash_from_file(file_path, hash_algo):
     """Returns hash of file at filePath at hashAlgo"""
     if not os.path.isfile(file_path):
         print(f"[-] Cannot open file at {file_path}")
