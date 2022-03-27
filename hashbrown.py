8b1cbeec3bad54d51184422d78c87e52e0e3cc67
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-03-26 19:01:52 -0500

    Added list of algorithms

--- hashbrown.py
+++ hashbrown.py
@@ -11,17 +11,12 @@ AUTHORS = ['_','_','Mason Eckenrod']
 if __name__ =='__main__':
     parser=argparse.ArgumentParser(
         description=f'''{PROG_NAME} is a python app that verifies current checksums with provided sum or online repository (Debian, Arch)''',
-        epilog="""Rowdy Hacks 2022 written by {', '.join(AUTHORS)}."""
+        epilog=f"""Rowdy Hacks 2022 written by {', '.join(AUTHORS)}."""
         )
     parser.add_argument('Path', type=str, default='/bin/bash', help='Path pointing to executable file or directory of executable files, or list of comma seperated files')
     parser.add_argument('--hash', type=str, default=None, help="Hash of the file used for checking. (Note. Repositories only provide md5sum for files)")
-    parser.add_argument('--hash-algo', '--algo', type=str, default='md5sum', help="Algorithm used for provided hash to verify against.")
+    parser.add_argument('--hash-algo', '--algo', type=str, default='md5sum', help=f"Algorithm used for provided hash to verify against. ALGOS:{', '.join(list_algos())}")
     args=parser.parse_args()
-
-    #TODO remove testing
-    print(args.Path)
-    print(args.hash)
-    #TODO end of testing
 
     # provided Path is a file
     if os.path.isfile(args.Path):
@@ -29,9 +24,9 @@ if __name__ =='__main__':
         
         result = get_file_hash(args.Path, args.hash_algo.lower())
         print(f'Decoding {args.Path} into {args.hash_algo}')
-        print()
+
     if os.path.isdir(args.Path):
         # call get_all_file_Hash
-
+        
         # get_dir_Hashes(args.Path, args.hash)
         print(f'Decoding files in {args.Path} to {args.hash}')
