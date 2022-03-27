d81e35c5daf4cbfac47fb6ffcac27c0c63f252b8
Isaiah <ifdsisd@gmail.com>
2022-03-26 19:15:00 -0500

    added arch hash check

---
+++ modules/arch_hash.py
@@ -0,0 +1,23 @@
+
+
+
+import subprocess
+
+def get_hash(bin:str):
+    "Retrieves the hash for a program using arch"
+    z
+    hash_check = subprocess.run("paccheck {}".format(bin).split(),stdout=subprocess.PIPE,encoding='utf-8')
+    info = {}
+    print(hash_check.returncode)
+    output = hash_check.stdout.split("\n")[:-1]
+    
+    
+      
+        
+    # print(info)
+    # print(info.keys())
+
+    
+
+if __name__ == "__main__":
+    get_hash("$(command -v htop)")
