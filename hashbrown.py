5c94da5250f837fca518e53dbcf48f3a4f79610a
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-03-27 11:26:06 -0500

    Rearranged only_hash and removed verbosity

--- hashbrown.py
+++ hashbrown.py
@@ -16,28 +16,29 @@ if __name__ =='__main__':
     parser.add_argument('--hash', '-x', type=str, default=None, help="Hash of the file used for checking. (Note. Repositories only provide md5sum for files)")
     parser.add_argument('--hash-algo', '--algo', '-a', type=str, default='md5', help=f"Algorithm used for provided hash to verify against. ALGOS: {', '.join(list_algos())}")
     parser.add_argument('--only-hash', '-O', action='store_true', help='Whether to only store the hashes of the files and not compare them, (Output in PATH:ALGO:HASH)')
-    # parser.add_argument('--script-file', '-f', action="store_true", help="Flag if Path points to file storing entries. (entries should be store as PATH:ALGO:HASH)")
+    parser.add_argument('--script-file', '-f', action="store_true", help="Flag if Path points to file storing entries. (entries should be store as PATH:ALGO:HASH)")
     parser.add_argument('--quiet', '-q', action='store_false', help="Enable verbose output")
     
     args=parser.parse_args()
 
+    # handle ONLY HASH
+    if args.only_hash:
+        cur_hash = get_hash(args.Path, args.hash_algo, False)
+        if isinstance(cur_hash, dict):
+            for k, v in cur_hash:
+                print(f"{k}:{args.hash_algo}:{v}")
+                exit(0)
+        if isinstance(cur_hash, str):
+            print(f"{args.Path}:{args.hash_algo}:{cur_hash}")
+            exit(0)
     
-    
-    # Call hash function
+    # handle SCRIPT FILE
     if args.script_file:
         #TODO Parse input file and call args
         pass
+    # handle BASE CASE
     else:
         cur_hash = get_hash(args.Path, args.hash_algo) 
-
-        if args.only_hash:
-            if isinstance(cur_hash, dict):
-                for k, v in cur_hash:
-                    print(f"{k}:{args.hash_algo}:{v}")
-                    exit(0)
-            if isinstance(cur_hash, str):
-                print(f"{args.Path}:{args.hash_algo}:{cur_hash}")
-                exit(0)
 
         if args.hash:
             exp_hash = args.hash
